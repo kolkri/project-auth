2b73f97f6fa1063f4a6352a4f6f944d80e3e0b8d
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-05-24 13:31:06 +0200

    started eith the backend, did the schema and model, endpoints for signin, login and authentication

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/project-auth-week20";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -12,13 +15,118 @@ mongoose.Promise = Promise;
 const port = process.env.PORT || 8080;
 const app = express();
 
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    // required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+// Authenticates user at login
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization");
+  try {
+    const user = await User.findOne({ accessToken });
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({
+        response: "You are not authorized, please log in/sign up",
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+};
+
+//Endpoint for logged in page
+app.get("/loginpage", authenticateUser);
+app.get("/loginpage", (req, res) => {
+  res.send("Hello Hippos");
+});
+
+app.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const salt = bcrypt.genSaltSync();
+
+    if (password.length < 8) {
+      throw "Password must be at least 8 characters long";
+    }
+
+    const newUser = await new User({
+      username,
+      email,
+      password: bcrypt.hashSync(password, salt),
+    }).save();
+
+    res.status(201).json({
+      response: {
+        userId: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        accessToken: newUser.accessToken,
+      },
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false,
+    });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userToFind = await User.findOne({ username });
+
+    if (userToFind && bcrypt.compareSync(password, userToFind.password)) {
+      res.status(201).json({
+        response: {
+          userId: newUser._id,
+          username: newUser.username,
+          email: newUser.email,
+          accessToken: newUser.accessToken,
+        },
+        success: true,
+      });
+    } else {
+      res.status(404).json({
+        response: "Username or password is incorrect.",
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false,
+    });
+  }
 });
 
 // Start the server
