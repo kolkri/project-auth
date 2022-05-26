b5f6005c9355e1d0b961769194f28b3a092f0747
Marianne Ardin <55622908+MarianneArdin@users.noreply.github.com>
2022-05-26 18:29:43 +0200

    fixing notfound page

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components/macro';
+
+const Title = styled.h1`
+	padding: 20px;
+	font-family: 'Montserrat';
+	font-style: normal;
+	font-weight: 700;
+	font-size: 24px;
+	line-height: 44px;
+	text-align: center;
+`;
+
+const Container = styled.div`
+	width: 300px;
+	height: 400px;
+	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+	padding: 20px;
+	margin: 2rem auto;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
 
 export const NotFound = () => {
-  return <h1>Sorry, not found!</h1>;
+	return (
+		<Container>
+			<Title>
+				<h1>404 page not found!</h1>
+				<Link to='/'>Go to Home </Link>
+			</Title>
+		</Container>
+	);
 };
