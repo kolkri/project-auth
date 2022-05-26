3bcfaa6dd262873df7f013e0b06fc91735c1f45a
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-05-26 12:13:11 +0200

    started with the form

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -7,6 +7,23 @@ import styled from "styled-components";
 import { API_URL } from "../utils/urls";
 import { user } from "../reducers/user";
 
+const Container = styled.div``;
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+// const Submitbutton = styled.button`
+//   background: lightgrey;
+//   border-radius: 3px;
+//   color: black;
+//   font-weight: bold;
+//   margin: 0 1em;
+//   padding: 0.25em 1em;
+//   width: 5rem;
+// `;
+
 export const Signup = () => {
   const [username, setUsername] = useState("");
   // // const [email, setEmail] = useState("");
@@ -17,6 +34,16 @@ export const Signup = () => {
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const onToggleClick = () => {
+    if (switchMode === "login") {
+      setSwitchMode("signup");
+      setIsContainerActive(true);
+    } else {
+      setSwitchMode("login");
+      setIsContainerActive(false);
+    }
+  };
 
   useEffect(() => {
     if (accessToken) {
@@ -61,45 +88,76 @@ export const Signup = () => {
   };
 
   return (
-    <>
-      <label htmlFor="signup">Sign up</label>
-      <input
-        id="signup"
-        type="radio"
-        checked={switchMode === "signup"}
-        onChange={() => setSwitchMode("signup")}
-      />
-      <label htmlFor="login">Log in</label>
-      <input
-        id="login"
-        type="radio"
-        checked={switchMode === "login"}
-        onChange={() => setSwitchMode("login")}
-      />
-      <form onSubmit={onFormSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        {/* <label htmlFor="email">Email</label>
+    <section>
+      <Container>
+        <FormContainer onSubmit={onFormSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {/* <label htmlFor="email">Email</label>
         <input
           id="email"
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         /> */}
-        <label htmlFor="password">Password</label>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {/* <Submitbutton type="submit">Submit</Submitbutton> */}
+          <button onClick={onToggleClick} id="signup">
+            signup
+          </button>
+          {/* <label htmlFor="signup">Sign up</label>
+          <input
+            id="signup"
+            type="radio"
+            checked={switchMode === "signup"}
+            onChange={() => setSwitchMode("signup")}
+          /> */}
+        </FormContainer>
+
+        <FormContainer onSubmit={onFormSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {/* <label htmlFor="email">Email</label>
         <input
-          id="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </>
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        /> */}
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {/* <Submitbutton type="submit">Login</Submitbutton> */}
+          <button onClick={onToggleClick} id="login">
+            login
+          </button>
+          {/* <label htmlFor="login">Log in</label>
+          <input
+            id="login"
+            type="radio"
+            checked={switchMode === "login"}
+            onChange={() => setSwitchMode("login")} 
+          /> */}
+        </FormContainer>
+      </Container>
+    </section>
   );
 };
