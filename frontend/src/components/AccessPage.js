804078511552c477e4f2768a77886e70dabc4a08
Mimmisen <mimmi.fordaluddin@hotmail.com>
2022-05-26 12:27:07 +0200

    merge 1

--- frontend/src/components/AccessPage.js
+++ frontend/src/components/AccessPage.js
@@ -3,15 +3,15 @@ import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { API_URL } from "../utils/urls";
 import { user } from "../reducers/user";
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Title = styled.h1`
-  font-family: 'Montserrat';
+  font-family: "Montserrat";
   font-style: normal;
   font-weight: 700;
   font-size: 36px;
   line-height: 44px;
-`
+`;
 
 const Main = styled.main`
   width: 100vw;
@@ -19,7 +19,7 @@ const Main = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Container = styled.div`
   width: 300px;
@@ -29,53 +29,53 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 const LogOutButton = styled.button`
   width: fit-content;
-`
+`;
 const ImageContainer = styled.div`
-  height:100%;
-  display:flex;
-  align-items:center;
-  justify-content:center;
-`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const ImageForm = styled.div`
-  width:250px;
-  padding:20px;
-  background:#fff;
-  border:2px dashed #555;
-`
+  width: 250px;
+  padding: 20px;
+  background: #fff;
+  border: 2px dashed #555;
+`;
 
 const ImageInput = styled.input`
-  display:none; 
-`
+  display: none;
+`;
 
 const ImageLabel = styled.label`
-  display:block;
-  width:100%;
-  height:50px;
-  line-height:50px;
-  text-align:center;
-  background:#333;
-  color:#fff;
-  font-size:15px;
-  font-family:"Open Sans",sans-serif;
-  text-transform:Uppercase;
-  font-weight:600;
-  border-radius:10px;
-  cursor:pointer;
-`
+  display: block;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  background: #333;
+  color: #fff;
+  font-size: 15px;
+  font-family: "Open Sans", sans-serif;
+  text-transform: Uppercase;
+  font-weight: 600;
+  border-radius: 10px;
+  cursor: pointer;
+`;
 
 const Image = styled.img`
-  width:100%;
-  display:none;
-  margin-top:10px;
-`
+  width: 100%;
+  display: none;
+  margin-top: 10px;
+`;
 export const AccessPage = () => {
   const [secret, setSecret] = useState("");
-  const [profileImg, setProfileImg] = useState(null)
+  const [profileImg, setProfileImg] = useState(null);
 
   const accessToken = useSelector((store) => store.user.accessToken);
   const dispatch = useDispatch();
@@ -110,19 +110,19 @@ export const AccessPage = () => {
   }, [accessToken]);
 
   const onProfilePicUpload = (event) => {
-    event.preventDefault()
-    console.log(event.target)
-  }
+    event.preventDefault();
+    console.log(event.target);
+  };
 
-const showPreview = (event) => {
-    event.preventDefault
-    if(event.target.files.length > 0){
+  const showPreview = (event) => {
+    event.preventDefault;
+    if (event.target.files.length > 0) {
       const src = URL.createObjectURL(event.target.files[0]);
       const preview = document.getElementById("profileImagePreview");
       preview.src = src;
       preview.style.display = "block";
     }
-  }
+  };
 
   return (
     <Main>
@@ -130,8 +130,15 @@ const showPreview = (event) => {
         <Title>{secret}</Title>
         <ImageContainer>
           <ImageForm>
-            <ImageLabel htmlFor="profileImage">Upload your profile image</ImageLabel>
-            <ImageInput type="file" id="profileImage" accept="image/*" onChange={showPreview} />
+            <ImageLabel htmlFor="profileImage">
+              Upload your profile image
+            </ImageLabel>
+            <ImageInput
+              type="file"
+              id="profileImage"
+              accept="image/*"
+              onChange={showPreview}
+            />
             <div>
               <Image id="profileImagePreview" />
             </div>
--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://project-auth-week-20.herokuapp.com";
+const BASE_URL = "https://project-auth-week-20.herokuapp.comgit";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
