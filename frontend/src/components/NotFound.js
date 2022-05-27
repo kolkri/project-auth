209216cfaebddaea26af2df682a90597e32b8e2a
Marianne Ardin <55622908+MarianneArdin@users.noreply.github.com>
2022-05-27 16:33:51 +0200

    new file for styled components+read me

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,27 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components/macro';
-
-const Title = styled.h1`
-	padding: 20px;
-	font-family: 'Montserrat';
-	font-style: normal;
-	font-weight: 700;
-	font-size: 24px;
-	line-height: 44px;
-	text-align: center;
-`;
-
-const Container = styled.div`
-	width: 300px;
-	height: 400px;
-	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-	padding: 20px;
-	margin: 2rem auto;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-`;
+import { Title, Container } from '../styledComponents/styledNotFound';
 
 export const NotFound = () => {
 	return (
